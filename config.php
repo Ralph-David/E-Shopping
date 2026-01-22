f161f049c0617626bc402a7191987b62dde37fbd
Ralph-Santillan <rdbsantillan@mymail.mapua.edu.ph>
2026-01-23 01:06:28 +0800

    committed tp sql-included

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 // Database configuration
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root'); // Change this to your MySQL username
-define('DB_PASS', ''); // Change this to your MySQL password
-define('DB_NAME', 'emart_db');
+define('DB_PASS', 'root1234'); // Change this to your MySQL password
+define('DB_NAME', 'emart');
 
 // Create connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
