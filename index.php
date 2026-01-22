f94d5305ca6ec038bf28f3c1144c920093c98ede
Jemle Serrano <delacruzjemle9@gmail.com>
2026-01-22 15:39:38 +0800

    Images added

--- index.php
+++ index.php
@@ -3,4 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Shopping</title>
+    <title>E-Mart</title>
+</head>
+<body>
+    <h1>Welcome to E-Mart</h1>
+    <p>Your one-stop shop for everything!</p>
+
+    
+</body>
+</html>
